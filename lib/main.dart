3e92cb9854d9d7959ef17f06b66af9085c4715c6
sangitadangal <sangita.dangal18@gmail.com>
2025-10-31 08:47:01 -0500

    added new homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'services/flashcard_provider.dart';
 import 'screens/sets_home_screen.dart';
 
 void main() {
@@ -10,20 +12,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'LeetCode Flashcards',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-        appBarTheme: const AppBarTheme(
-          centerTitle: false,
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-          elevation: 2,
+    return ChangeNotifierProvider(
+      create: (context) => FlashcardProvider(),
+      child: MaterialApp(
+        title: 'LeetCode Flashcards',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+          appBarTheme: const AppBarTheme(
+            centerTitle: false,
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+            elevation: 2,
+          ),
         ),
+        home: const SetsHomeScreen(),
       ),
-      home: const SetsHomeScreen(),
     );
   }
 }
